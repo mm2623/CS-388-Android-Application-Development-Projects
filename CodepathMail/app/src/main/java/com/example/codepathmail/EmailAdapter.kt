039e057f296aaff767f825cb6779aaaccbe2f4f5
mm2623 <mm2623@njit.edu>
2023-02-02 19:54:36 -0500

    Code path Mail

---
+++ CodepathMail/app/src/main/java/com/example/codepathmail/EmailAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.codepathmail
+
+import android.graphics.Paint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class EmailAdapter(private val emails: List<Email>) : RecyclerView.Adapter<EmailAdapter.ViewHolder>() {
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        // TODO: Create member variables for any view that will be set
+        val senderTextView: TextView
+        val titleTextView: TextView
+        val summaryTextView: TextView
+
+        init {
+            // TODO: Store each of the layout's views into
+            senderTextView = itemView.findViewById(R.id.sender)
+            senderTextView.paintFlags = senderTextView.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+            titleTextView = itemView.findViewById(R.id.title)
+            summaryTextView = itemView.findViewById(R.id.summary)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.email_item, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    override fun getItemCount(): Int {
+        return emails.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get the data model based on position
+        val email = emails.get(position)
+        // Set item views based on views and data model
+        holder.senderTextView.text = email.sender
+        holder.titleTextView.text = email.title
+        holder.summaryTextView.text = email.summary
+    }
+}
